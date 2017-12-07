a2a37065e3325175b68d707331645a37c6d73762
Brandon Leeds <brandonleeds@gmail.com>
2017-12-07 01:36:22 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each { |key, detail_hash|
     puts "#{key.capitalize}:"
     detail_hash.each { |key, value|
-      puts "#{key}: #{value.join(" ,")}"
+      puts "#{key.capitalize}: #{value.join(", ")}"
     }
   }
 end
