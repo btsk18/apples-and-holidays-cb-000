6cef030f923b5ec7d4ac43bb1b50631670a2bb25
Brandon Leeds <brandonleeds@gmail.com>
2017-12-07 01:34:25 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,9 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each { |key, detail_hash|
     puts "#{key}:"
-    detail_hash.each { |key, value| value << supply } if key == :spring
+    detail_hash.each { |key, value| 
+      puts "#{key}: #{value}"
+    }
   }
 end
 
